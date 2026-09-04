2caa73a81c04e91e0864190a8ee390e4bba7651e
jonetwelve <jonetwelve@hotmail.com>
2026-09-04 11:03:17 +0800

    feat:添加chainsaw插件,project插件写帮助文档

---
+++ lua/custom/chainsaw.lua
@@ -0,0 +1,23 @@
+pack_add({
+
+	"chrisgrieser/nvim-chainsaw",
+})
+
+require("chainsaw").setup({
+	logStatements = {
+		variableLog = {
+			javascript = 'console.info("{{marker}} {{lnum}} {{var}}:", {{var}});',
+		},
+		-- the same for the other log statement operations
+	},
+})
+
+nvim.key.map("n", "<leader>llv", "<cmd>Chainsaw variableLog<cr>", { desc = "log variable" })
+nvim.key.map("n", "<leader>llo", "<cmd>Chainsaw objectLog<cr>", { desc = "log objectLog" })
+nvim.key.map("n", "<leader>llt", "<cmd>Chainsaw typeLog<cr>", { desc = "log type" })
+nvim.key.map("n", "<leader>lla", "<cmd>Chainsaw assertLog<cr>", { desc = "log assert" })
+nvim.key.map("n", "<leader>lle", "<cmd>Chainsaw emojiLog<cr>", { desc = "log emoji" })
+nvim.key.map("n", "<leader>lls", "<cmd>Chainsaw soundLog<cr>", { desc = "log sound" })
+nvim.key.map("n", "<leader>llm", "<cmd>Chainsaw messageLog<cr>", { desc = "log message" })
+nvim.key.map("n", "<leader>llc", "<cmd>Chainsaw clearLog<cr>", { desc = "log clear" })
+nvim.key.map({ "n", "v" }, "<leader>llr", "<cmd>Chainsaw removeLogs<cr>", { desc = "log remove" })
--- lua/custom/project.lua
+++ lua/custom/project.lua
@@ -18,3 +18,34 @@ require("telescope").setup({
 nvim.key.map("n", "<leader>p", function()
 	require("telescope").extensions.project.project({})
 end, { desc = "项目" })
+
+require("help").add(
+	"project/normal-mode",
+	[[
+Key 	Description
+d 	delete currently selected project
+r 	rename currently selected project
+c 	create a project*
+s 	search inside files within your project
+b 	browse inside files within your project
+w 	change to the selected project's directory without opening it
+R 	find a recently opened file within your project
+f 	find a file within your project (same as <CR>)
+o 	change current cd scope
+]]
+)
+require("help").add(
+	"project/insert-mode",
+	[[
+Key 	Description
+<c-d> 	delete currently selected project
+<c-v> 	rename currently selected project
+<c-a> 	create a project*
+<c-s> 	search inside files within your project
+<c-b> 	browse inside files within your project
+<c-l> 	change to the selected project's directory without opening it
+<c-r> 	find a recently opened file within your project
+<c-f> 	find a file within your project (same as <CR>)
+<c-o> 	change current cd scope
+]]
+)
