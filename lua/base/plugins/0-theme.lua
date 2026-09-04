4560845f794874110de6dac566cdac9d5f8ce911
jonetwelve <jonetwelve@hotmail.com>
2026-09-04 15:23:23 +0800

    refactor:修改主题

--- lua/base/plugins/0-theme.lua
+++ lua/base/plugins/0-theme.lua
@@ -1,15 +1,3 @@
-pack_add("folke/tokyonight.nvim")
+pack_add("catppuccin/nvim")
 
-require("tokyonight").setup({
-	style = "day", -- Dynamically set based on selected_theme
-	transparent = false,
-	terminal_colors = true,
-	styles = {
-		comments = { italic = true },
-		keywords = { italic = true },
-		functions = {},
-		variables = {},
-	},
-})
-
-vim.cmd("colorscheme tokyonight-night")
+vim.cmd("colorscheme catppuccin-mocha")
